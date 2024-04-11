fa5d0163d691080007991bb8c9f3162549823c21
Jakub Poddemski <247765@edu.p.lodz.pl>
2024-04-11 20:27:08 +0200

    Dodanie metody Moving() dla klasy ball

--- Logic/Ball.cs
+++ Logic/Ball.cs
@@ -8,18 +8,18 @@ namespace Logic
 {
     public class Ball
     {
-        public double X { get; set; }
-        public double Y { get; set; }
-        public double VelocityX { get; set; }
-        public double VelocityY { get; set; }
+        public double x { get; set; }
+        public double y { get; set; }
+        public double velocityX { get; set; }
+        public double velocityY { get; set; }
         public double r {  get; set; }
 
         public Ball()
         {
-            X = getRandomNumber(10, 100);
-            Y = getRandomNumber(10, 100);
-            VelocityX = getRandomNumber(10, 100);   
-            VelocityY = getRandomNumber(10, 100);
+            x = getRandomNumber(10.0, 100.0);
+            y = getRandomNumber(10.0, 100.0);
+            velocityX = getRandomNumber(10, 100);   
+            velocityY = getRandomNumber(10, 100);
 
             r = 5;
         }
@@ -28,6 +28,23 @@ namespace Logic
         public double getRandomNumber(double min, double max)
         {
             return random.NextDouble() * (max - min) + min;  
+        }
+
+        public void Moving(double height, double width)
+        {
+            double next_x = x + velocityX;
+            double next_y = y + velocityY;
+
+            if (next_x + r > width || next_x < r)
+            {
+                velocityX *= -1.0;
+            }
+            if (next_y + r > height || next_y < r)
+            {
+                velocityY *= -1.0;
+            }
+            x = next_x;
+            y = next_y;
         }
 
 
