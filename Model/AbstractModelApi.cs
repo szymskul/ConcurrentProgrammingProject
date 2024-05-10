10f36268cc5f1d25811d4bc1a734ed0593dd4f0b
Jakub Poddemski <247765@edu.p.lodz.pl>
2024-05-10 12:02:27 +0200

    Model layer modification

--- Model/AbstractModelApi.cs
+++ Model/AbstractModelApi.cs
@@ -2,12 +2,26 @@
 using Logic;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Reactive;
 
 namespace Model
 {
-    public abstract class ModelAbstractApi
+    public interface IBall : INotifyPropertyChanged
     {
-        public abstract List<IModelBall> balls { get; }
+        double Top { get; }
+        double Left { get; }
+        int Diameter { get; }
+    }
+
+    public class BallChaneEventArgs : EventArgs
+    {
+        public IBall Ball { get; set; }
+    }
+
+    public abstract class ModelAbstractApi : IObservable<IBall>
+    {
 
         public abstract void GenerateBalls(int counts);
 
@@ -15,35 +29,44 @@ namespace Model
         {
             return new ModelLayerApi();
         }
+        #region IObservable
+
+        public abstract IDisposable Subscribe(IObserver<IBall> observer);
+        #endregion IObservable
+
     }
 
     internal class ModelLayerApi : ModelAbstractApi
     {
         private LogicAbstractAPI logicApi;
-
-        public override List<IModelBall> balls => copyBallList();
+        public event EventHandler<BallChaneEventArgs> BallChanged;
+        private IObservable<EventPattern<BallChaneEventArgs>> eventObservable = null;
+        private List<BallModel> Balls = new List<BallModel>();
 
         public ModelLayerApi()
         {
             logicApi = logicApi ?? LogicAbstractAPI.CreateAPI();
+            IDisposable observer = logicApi.Subscribe(x => Balls[x.Id - 1].Move(x.Position));
+            eventObservable = Observable.FromEventPattern<BallChaneEventArgs>(this, "BallChanged");
         }
 
         public override void GenerateBalls(int counts)
         {
-            logicApi.createBalls(counts);
-            logicApi.start();
+            //logicApi.createBalls(counts);
+            //logicApi.start();
+            for (int i = 0; i < counts; i++)
+            {
+                BallModel newBall = new BallModel(0, 0, 15);
+                Balls.Add(newBall);
+                BallChanged?.Invoke(this, new BallChaneEventArgs() { Ball = newBall });
+            }
+            logicApi.AddStart(counts);
 
         }
 
-        public List<IModelBall> copyBallList()
+        public override IDisposable Subscribe(IObserver<IBall> observer)
         {
-            List<IModelBall> ballsModel = new List<IModelBall>();
-
-            foreach (IBall b in logicApi.getAllBalls())
-            {
-                ballsModel.Add(new BallModel(b));
-            }
-            return ballsModel;
+            return eventObservable.Subscribe(x => observer.OnNext(x.EventArgs.Ball), ex => observer.OnError(ex), () => observer.OnCompleted());
         }
     }
 }
--- Model/BallModel.cs
+++ Model/BallModel.cs
@@ -1,24 +1,91 @@
-﻿using Logic;
+﻿using Data;
+using Logic;
+using System.ComponentModel;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace Model
 {
-    internal class BallModel : IModelBall
+    internal class BallModel : IBall
     {
-        private IBall ball;
+        //private IBall ball;
 
-        public BallModel(IBall ball)
+        ///public BallModel(IBall ball)
+        //{
+        //this.ball = ball;
+        //}
+
+        //public override double Xposition
+        //{
+        //get { return ball.X; }
+        //}
+
+        //public override double Yposition
+        //{
+        //get { return ball.Y; }
+        //}
+        public int Diameter { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double top;
+        private double left;
+
+        public BallModel(double top, double left, int radius)
         {
-            this.ball = ball;
+            Top = top;
+            Left = left;
+            Diameter = radius * 2;
         }
 
-        public override double Xposition
+        public double Top
         {
-            get { return ball.X; }
+            get { return top; }
+            set
+            {
+                if (top == value)
+                    return;
+                top = value;
+                RaisePropertyChanged();
+            }
         }
 
-        public override double Yposition
+
+        public double Left
         {
-            get { return ball.Y; }
+            get { return left; }
+            set
+            {
+                if (left == value)
+                    return;
+                left = value;
+                RaisePropertyChanged();
+            }
         }
+
+        public void Move_V(Vector2 position)
+        {
+            Left = position.X;
+            Top = position.Y;
+        }
+
+        public void Move(double poitionX, double positionY)
+        {
+            Left = poitionX;
+            Top = positionY;
+        }
+
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public void Move(Vector2 position)
+        {
+            Left = position.X;
+            Top = position.Y;
+        }
+
+
     }
 }
