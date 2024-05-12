1d1439dbe3a9f2c05a181261dbfde8b25b68dd5b
szymskul <szymskul@gmail.com>
2024-05-12 22:30:49 +0200

    small update

--- ViewModel/UserCommand.cs
+++ ViewModel/UserCommand.cs
@@ -5,15 +5,11 @@ using System.Windows.Input;
 
 namespace ViewModel
 {
-    // Klasa UserCommand pozwala na reakcję na akcje użytkownika w interfejsie użytkownika.
     internal class UserCommand : ICommand
     {
-        // Pole execute przechowuje metodę, która zostanie wykonana po wywołaniu komendy.
         private readonly Action execute;
-        // Pole canExecute to funkcja boolowska, która określa, czy można wywołać tę komendę.
         private readonly Func<bool> canExecute;
 
-        // Zdarzenie CanExecuteChanged pozwala na odświeżenie widoku, gdy zmienia się możliwość wywołania komendy.
         public event EventHandler CanExecuteChanged;
 
         public UserCommand(Action execute, Func<bool> canExecute)
@@ -23,31 +19,24 @@ namespace ViewModel
         }
 
         public UserCommand(Action execute) : this(execute, null) { }
-
-        // Implementacja metody Execute interfejsu ICommand, która wywołuje metodę execute.
         public virtual void Execute(object obj)
         {
             this.execute();
         }
 
-        // Implementacja metody CanExecute interfejsu ICommand, która sprawdza, czy można wywołać komendę.
         public bool CanExecute(object obj)
         {
             if (this.canExecute == null)
             {
-                // Jeśli canExecute jest puste, zwraca true.
                 return true;
             }
             if (obj == null)
             {
-                // Jeśli obj jest puste, zwraca wartość canExecute.
                 return this.canExecute();
             }
-            // Zwraca wartość canExecute.
             return this.canExecute();
         }
 
-        // Metoda RaiseCanExecuteChanged służy do wywołania zdarzenia CanExecuteChanged.
         public void RaiseCanExecuteChanged()
         {
             this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
